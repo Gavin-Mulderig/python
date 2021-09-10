fd8414b4c8971228e2fe78bfbb0fdb7717f431b3
Gavin-Mulderig <73673305+Gavin-Mulderig@users.noreply.github.com>
2021-09-10 09:01:43 -0700

    Add files via upload

---
+++ variables1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#save file as variables.py
+# s strings
+# i integer
+# f float
+s = input("Input a string: ")
+i = int(input("Input an integer: "))
+f = float(input("Input a float: "))
+
+print(s, i, f)
+print(str(s)+" "+str(i)+" "+str(f))
